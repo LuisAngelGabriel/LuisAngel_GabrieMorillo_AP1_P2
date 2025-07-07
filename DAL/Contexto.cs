5a3e09c6b0deaca8be5ae3a9266e7627ee7f53ab
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-07-07 19:34:55 -0400

    Creando el contexto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LuisAngel_GabrieMorillo_AP1_P2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    }
+}
