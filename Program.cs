a77d476e28b5dba3a72028a7f78c0b81509d48b7
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-07-07 19:35:56 -0400

    Inyectando el contexto y el service.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
+using Blazored.Toast;
 using LuisAngel_GabrieMorillo_AP1_P2.Components;
+using LuisAngel_GabrieMorillo_AP1_P2.DAL;
+using LuisAngel_GabrieMorillo_AP1_P2.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+builder.Services.AddBlazoredToast();
+
+builder.Services.AddScoped<DetallesService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
