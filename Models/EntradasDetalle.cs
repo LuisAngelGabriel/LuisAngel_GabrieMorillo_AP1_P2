c1fea4999d628c84204c1da7a12d43569c23ebb4
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-07-14 21:18:28 -0400

    Creando los modelos.

---
+++ Models/EntradasDetalle.cs
@@ -0,0 +1,15 @@
+﻿namespace LuisAngel_GabrieMorillo_AP1_P2.Models
+{
+    public class EntradasDetalle
+    {
+
+        public int Id { get; set; }
+        public int EntradaId { get; set; }
+        public Entradas? Entrada { get; set; }
+
+        public int ProductoId { get; set; }
+        public Productos? Producto { get; set; }
+
+        public double Cantidad { get; set; }
+    }
+}
